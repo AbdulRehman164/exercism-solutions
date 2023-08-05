ea916b84126cf84f145bebaf6038ef321a4644f6
Abdul Rehman <abdulrehmang571@gmail.com>
2023-08-05 18:13:23 +0500

    completed the lasagna-master exercise

--- javascript/lasagna-master/lasagna-master.js
+++ javascript/lasagna-master/lasagna-master.js
@@ -10,3 +10,37 @@
  *   ...
  * }
  */
+
+export function cookingStatus(timer) {
+  if (timer === 0) return 'Lasagna is done.';
+  else if (timer === undefined) return 'You forgot to set the timer.';
+  else return 'Not done, please wait.';
+}
+
+export function preparationTime(layers, time = 2) {
+  return layers.length * time;
+}
+
+export function quantities(layers) {
+  const QUANTITIES_OBJ = {
+    noodles: 0,
+    sauce: 0,
+  };
+  layers.forEach((layer) => {
+    if (layer === 'noodles') QUANTITIES_OBJ.noodles += 50;
+    if (layer === 'sauce') QUANTITIES_OBJ.sauce += 0.2;
+  });
+  return QUANTITIES_OBJ;
+}
+
+export function addSecretIngredient(friendsList, myList) {
+  myList.push(friendsList[friendsList.length - 1]);
+}
+
+export function scaleRecipe(recipie, portions) {
+  const TEMP = { ...recipie };
+  for (let key in TEMP) {
+    TEMP[key] *= portions / 2;
+  }
+  return TEMP;
+}
