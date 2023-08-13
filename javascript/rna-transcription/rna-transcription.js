985b441e03fcf4afd5b4dbd614677ff4988a6797
Abdul Rehman <abdulrehmang571@gmail.com>
2023-08-13 19:48:55 +0500

    completed rna transcription

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -3,6 +3,16 @@
 // convenience to get you started writing code faster.
 //
 
-export const toRna = () => {
-  throw new Error('Remove this statement and implement this function');
+const DNA_TO_RNA = {
+  G: 'C',
+  C: 'G',
+  T: 'A',
+  A: 'U',
+};
+
+export const toRna = (dnaStrands) => {
+  return dnaStrands
+    .split('')
+    .map((strand) => DNA_TO_RNA[strand])
+    .join('');
 };
